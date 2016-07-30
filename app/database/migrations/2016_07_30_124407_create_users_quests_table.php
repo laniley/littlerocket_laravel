b1d7c1b9a13def0a8b8b2918ff6a3a0e30bc5804
laniley <melanie.mende@me.com>
2016-07-30 15:04:02 +0200

    close #8

---
+++ app/database/migrations/2016_07_30_124407_create_users_quests_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersQuestsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users_quests', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->integer('user_id')->unsigned();
+			$table->integer('quest_id')->unsigned();
+
+			$table->boolean('reward_has_been_collected')->default(false);
+
+			$table->timestamp('completed_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('quest_id')->references('id')->on('quests');
+
+			$table->unique(['user_id', 'quest_id']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users_quests');
+	}
+
+}
