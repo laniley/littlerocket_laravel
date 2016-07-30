feb7b70c0354d10a2cf42f3fe371419c2e1469c8
laniley <melanie.mende@me.com>
2016-07-30 15:03:22 +0200

    close #4

---
+++ app/database/migrations/2016_07_30_120000_create_quest_reward_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestRewardTypesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('quest_reward_types', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+
+			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('quest_reward_types');
+	}
+
+}
