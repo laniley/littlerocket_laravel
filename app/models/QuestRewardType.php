444270fa4d98146654077668af5e98f710c9cc5c
laniley <melanie.mende@me.com>
2016-07-31 10:19:15 +0200

    close #10

---
+++ app/models/QuestRewardType.php
@@ -0,0 +1,20 @@
+<?php
+
+class QuestRewardType extends Eloquent{
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'quest_reward_types';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('created_at', 'updated_at');
+
+	// protected $fillable = array('user_id', 'cannon_id', 'shield_id', 'engine_id');
+}
